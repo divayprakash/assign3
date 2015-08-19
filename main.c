610357286939caaf2c167020534fc13d49b40b0a
Divay Prakash <divay@divay-Inspiron-N5050>
2015-08-19 11:40:55 +0530

    Working; with documentation

--- main.c
+++ main.c
@@ -1,35 +1,20 @@
 #include <stdint.h>
 
 #define _MMIO_BYTE(mem_addr) (*(volatile uint8_t *)(mem_addr))
-
 #define _SFR_IO8(io_addr) _MMIO_BYTE((io_addr) + __SFR_OFFSET)
 #define __SFR_OFFSET 0x20
-
+// macros for setting register values and offsets
 #define DDRB _SFR_IO8(0x04)
-#define DDB0 0
-#define DDB1 1
-#define DDB2 2
-#define DDB3 3
-#define DDB4 4
-#define DDB5 5
-#define DDB6 6
-#define DDB7 7
-
+// define DDRB register
 #define PORTB _SFR_IO8(0x05)
-#define PORTB0 0
-#define PORTB1 1
-#define PORTB2 2
-#define PORTB3 3
-#define PORTB4 4
-#define PORTB5 5
-#define PORTB6 6
-#define PORTB7 7
-
+// define PORTB
 #define DELAY_MS 1000
 
 void delay_func(uint16_t ms) 
 {
+	//function to implement delay
 	uint16_t delay_count = F_CPU / 16000;
+	//set delay count to 1000ms
 	volatile uint16_t i;
 	while (ms != 0) 
 	{
